689fcb485a4b172fe9b72b28394ce3ca7efb1530
trogdor <trog@trogdors-MacBook-Air.local>
2024-05-17 16:07:47 -0700

    etc etc wip

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.widgets import Button, Header, Footer, Static
+from textual.containers import ScrollableContainer
+
+
+class Plugin(Static):
+    def compose(self) -> ComposeResult:
+        yield Button("Start", id="start", variant="success")
+        yield Button("Stop", id="stop", variant="error")
+        yield Button("Reset", id="reset")
 
 
 class BlyatApp(App):
+    CSS_PATH = "main.tcss"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield ScrollableContainer(Plugin(), Plugin(), Plugin(), id="timers")
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
